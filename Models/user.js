437fbe883c8893b5c699feef5c3642dc2c0b9b5d
Aditya Thakur <adityathakur.mca18.du@gmail.com>
2023-03-02 12:49:54 +0530

    Database connection

---
+++ Models/user.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 const port = 3000
 const app = express()
+
+
+mongoose.connect('mongodb+srv://adityathakurmca18du:QOsd5XjYIHS5aaRH@cluster0.ppjbz6k.mongodb.net/test', {useNewUrlParser: true})
+mongoose.connection.once('open',function(){
+    console.log('Database connected Successfully');
+  }).on('error',function(err){
+    console.log('Error', err);
+  })
 
 app.listen(port,()=>{
     console.log("Listening on port: ${port}")
